757afb6158f18e5528344c1f2316c0e71bfb6261
talitaprimo <talitareisprimo@gmail.com>
2020-07-06 11:14:18 -0300

    Estudo do método post e das anotações Builder e Data

---
+++ src/main/java/projetodeestudo/domains/Product.java
@@ -0,0 +1,16 @@
+package projetodeestudo.domains;
+
+import com.sun.istack.internal.NotNull;
+import lombok.*;
+@Builder
+@Getter
+@EqualsAndHashCode(of = "sku")
+@ToString(of={"name", "price"})
+public class Product {
+    @NotNull
+    private final String name;
+    @NotNull
+    private final String sku;
+    private final double price;
+
+}
--- src/main/java/projetodeestudo/gateways/http/Controller.java
+++ src/main/java/projetodeestudo/gateways/http/Controller.java
@@ -1,8 +1,10 @@
 package projetodeestudo.gateways.http;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import projetodeestudo.domains.Product;
 
-import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/teste")
@@ -25,5 +27,36 @@ public class Controller {
         return nome+" " +endereco+" "+telefone+" "+receberNotificacao;
     }
 
+//    @GetMapping("/Produto")
+//    public String getProduct(){
+//
+//        Product product = new Product("tênis", "123-3456-8765-098");
+//        product.setPrice(250);
+//        return product.toString();
+//    }
+
+    @PostMapping("/compare")
+    public boolean compare(@RequestBody List<Product> products) {
+        return products.get(0).equals(products.get(1));
+    }
+
+//    @GetMapping("/reference")
+//    public boolean getReference(){
+//
+//        Product productA = new Product("tênis", "123-3456-8765-098");
+//        productA.setPrice(250);
+//        Product productB = new Product("tênis1", "123-3456-8765-098");
+//        productB.setPrice(250);
+//        return productA.equals(productB);
+//
+//    }
+
+    @GetMapping("/produtoBuilder")
+    public Product productBuilder() {
+        return Product.builder()
+                .name("sapato")
+                .sku("123-321")
+                .build();
+    }
     //required = true é padrão
 }
